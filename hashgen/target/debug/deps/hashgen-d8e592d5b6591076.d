117c1dc7eb004d42c84feeab5919df671c6f5906
Meridian Academy <meridian@academy.dev>
2026-04-06 02:06:05 +0300

    Remove hashgen permanently and enforce gitignore

--- hashgen/target/debug/deps/hashgen-d8e592d5b6591076.d
+++
@@ -1,5 +0,0 @@
-/home/eren/Documents/task4/Meridian_Academy/hashgen/target/debug/deps/hashgen-d8e592d5b6591076.d: src/main.rs
-
-/home/eren/Documents/task4/Meridian_Academy/hashgen/target/debug/deps/hashgen-d8e592d5b6591076: src/main.rs
-
-src/main.rs:
--- hashgen/target/debug/hashgen.d
+++
@@ -1,1 +0,0 @@
-/home/eren/Documents/task4/Meridian_Academy/hashgen/target/debug/hashgen: /home/eren/Documents/task4/Meridian_Academy/hashgen/src/main.rs
